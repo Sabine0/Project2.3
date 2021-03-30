6c426d7875d6a36fe3cdd3bf1003817fc655c19c
luc9willemse <lucwillemse@live.nl>
2021-03-30 09:56:33 +0200

    implementend board creater

--- src/algorithm/ReversieAlgorithm.java
+++ src/algorithm/ReversieAlgorithm.java
@@ -1,5 +1,14 @@
 package algorithm;
 
+import java.util.HashMap;
+
 public class ReversieAlgorithm {
-    
+
+    private int[] board;
+
+    public ReversieAlgorithm (int[] board) {
+        this.board = board;
+    }
+
+    public HashMap<Integer, int[]> 
 }
