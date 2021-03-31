25c5085d7fa8ded28b3cdb320248fac3206d1e7d
yljh1 <57140266+yljh1@users.noreply.github.com>
2021-03-31 12:59:31 +0200

    begin ttt algo

--- src/app/algorithm/TicTacToeAlgorithm.java
+++ src/app/algorithm/TicTacToeAlgorithm.java
@@ -1,5 +1,64 @@
-package app.algorithm;
+package algorithm;
+
+import java.util.Random;
 
 public class TicTacToeAlgorithm {
-    
+
+    private int[] board;
+    Random rand = new Random();
+    static final int[] corners = {0, 2, 6, 8};
+
+    public void setBoard (int[] board) {
+        this.board = board;
+    }
+
+    //in deze methode wordt het spel gespeeld
+    // geef mee wie er aan de beurt is
+    public void game(boolean beurt, int[] board){
+        //met deze ints wordt bepaald op welke plaats de speler een set doet
+        int setSelf;
+        int setOpp;
+
+        //initialize het board
+        setBoard(board);
+
+        //als jij de eerste stap mag maken, kies dan een random hoek uit
+        if(beurt){
+            setSelf = getCorner(corners);
+            board[setSelf] = 1;
+            beurt = false;
+        }
+        else{
+            switch(getPosition()){
+                case 0, 2, 6, 8:
+                    //
+                    break;
+                case 4:
+                    //
+                    break;
+                default:
+                    //
+                    break;
+            }
+        }
+
+    }
+
+    //met deze methode wordt een hoek uitgekozen waar een set op gedaan kan worden
+    private int getCorner(int[] array){
+        int corner = rand.nextInt(array.length);
+        return array[corner];
+    }
+
+    private void getPossibility(){
+        final int self = 1;
+        final int opp = 2;
+    }
+
+    private int getPosition(){
+        int pos;
+
+        return pos;
+    }
+
 }
