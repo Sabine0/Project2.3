6b2f37dfd22d14cc62c6c24942a7076a36c2a362
yljh1 <57140266+yljh1@users.noreply.github.com>
2021-04-14 21:44:13 +0200

    Update TicTacToeAI.java
    nog niet kunnen testen

--- src/app/users/TicTacToeAI.java
+++ src/app/users/TicTacToeAI.java
@@ -3,21 +3,45 @@ package app.users;
 import app.users.Bot;
 import app.view.gameobjects.Board;
 
+import java.util.ArrayList;
+import java.util.Random;
+
 /**
  * The TicTacToeAI class holds the attributes of the tictactoe algorithm
  * @author Sabine Schreuder
  * @version 13-04-21
  */
-public class TicTacToeAI extends Bot{
-    public TicTacToeAI(String username){
+public class TicTacToeAI extends Bot {
+    public TicTacToeAI(String username) {
         super(username);
     }
 
-    public int[] getMove(Board board) {
-        //implement algorithm here!!
-        int[] array = new int[2];
+    boolean first = true;
 
-        // will return int[]
-        return array;
+    public int[] getMove(Board board) {
+
+        int[] corners = {0, 2};
+        Random rand = new Random();
+        int X;
+        int Y;
+        int[] coordinates = new int[]{};
+
+        if (first) {
+            X = rand.nextInt(corners.length);
+            Y = rand.nextInt(corners.length);
+            first = false;
+
+        } else {
+            X = rand.nextInt(3);
+            Y = rand.nextInt(3);
+        }
+
+        if(board.isValidMove(X,Y)){
+            coordinates = new int[]{corners[X], corners[Y]};
+        }
+        else{
+            getMove(board);
+        }
+        return coordinates;
     }
 }
